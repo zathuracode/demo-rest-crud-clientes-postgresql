12f7b10467f58e5c894a6c7622d2ccc58fcbbd4a
Diego Armando Gomez <dgomez@vortexbird.com>
2017-07-17 14:58:07 -0500

    Carga inicial

---
+++ index.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+
+const cn = {
+    host: 'localhost', // 'localhost' is the default;
+    port: 5432, // 5432 is the default;
+    database: 'puppies',
+    user: 'postgres',
+    password: 'admin',
+    poolSize:10
+};
+// You can check for all default values in:
+// https://github.com/brianc/node-postgres/blob/master/lib/defaults.js
+
+const db = pgp(cn); // database instance;
+
+// add query functions
+
+var app = express();
+
+app.get('/api/puppies', function (req, res,next) {
+  db.any('select * from pups')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL puppies'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+});
+
+app.get('/api/puppies/:id', function (req, res, next) {
+  var pupID = parseInt(req.params.id);
+  db.one('select * from pups where id = $1', pupID)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ONE puppy'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+});
+
+app.post('/api/puppies', function (req, res, next) {
+  req.body.age = parseInt(req.body.age);
+  db.none('insert into pups(name, breed, age, sex)' +
+      'values(${name}, ${breed}, ${age}, ${sex})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted one puppy'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+});
+
+app.put('/api/puppies/:id', function (req, res, next) {
+  db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
+    [req.body.name, req.body.breed, parseInt(req.body.age),
+      req.body.sex, parseInt(req.params.id)])
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Updated puppy'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+});
+
+app.delete('/api/puppies/:id', function (req, res, next) {
+  var pupID = parseInt(req.params.id);
+  db.result('delete from pups where id = $1', pupID)
+    .then(function (result) {
+      /* jshint ignore:start */
+      res.status(200)
+        .json({
+          status: 'success',
+          message: `Removed ${result.rowCount} puppy`
+        });
+      /* jshint ignore:end */
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+});
+
+app.listen(3000, function () {
+  console.log('Server port 3000!')
+})
